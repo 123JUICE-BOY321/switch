28ebe3cb2a4970730e6900e8e8cc0aaf8e279205
Tanishq Shreeyas <tanishqshreeyas_sunkara@srmap.edu.in>
2026-04-15 01:18:34 +0530

    Add CORS middleware to FastAPI application
    Added CORS middleware to allow cross-origin requests.

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from datetime import datetime, timezone
 from pymongo import MongoClient
 import os
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 MONGO_URL = os.getenv("MONGO_URL")
 client = MongoClient(MONGO_URL)
 db = client["switchdb"]
